3c07169df7d403088857f8d2cd16aa1b606de273
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-17 23:51:51 +0530

    Add MeetingTypeList and HomeCard components, integrate into home page with dynamic date and time display

---
+++ components/HomeCard.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+interface HomeCardProps {
+  className: string;
+  img: string;
+  title: string;
+  description: string;
+  handleClick?: () => void;
+}
+
+const HomeCard = ({
+  className,
+  img,
+  title,
+  description,
+  handleClick,
+}: HomeCardProps) => {
+  return (
+    <div
+      className={cn(
+        "bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+        className
+      )}
+      onClick={handleClick}
+    >
+      <div className="flex flex-center glassmorphism size-12 rounded-[10px]">
+        <Image src={img} height={27} width={27} alt="Meeting" />
+      </div>
+      <h1 className="text-2xl font-bold">{title}</h1>
+      <p className="text-lg font-normal">{description}</p>
+    </div>
+  );
+};
+
+export default HomeCard;
