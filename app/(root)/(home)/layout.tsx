48e11e4fc0376fd3f31cc54131b0b239592c12c8
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-17 13:22:02 +0530

    Sidebar complete

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,12 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <main className="relative">
-        NavBar
+        <Navbar />
         <div className="flex">
-          Sidebar
+          <Sidebar />
           <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
             <div className="w-full">{children}</div>
           </section>
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/Upcoming",
+    imgUrl: "/icons/Upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/Previous",
+    imgUrl: "/icons/Previous.svg",
+  },
+  {
+    label: "Recording",
+    route: "/Recording",
+    imgUrl: "/icons/recordings.svg",
+  },
+  {
+    label: "Personal",
+    route: "/Personal",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
