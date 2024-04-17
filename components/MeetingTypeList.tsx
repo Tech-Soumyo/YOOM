8c325580dce2fe154a00d51d385196bc7d177056
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-18 02:38:21 +0530

    Add Dialog component and integrate MeetingModel into MeetingTypeList for instant meeting functionality

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,12 +2,15 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModel";
 
 function MeetingTypeList() {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+
+  const createMeeting = () => {};
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -37,6 +40,14 @@ function MeetingTypeList() {
         description="Meeting Recordings"
         className="bg-yellow-1"
         handleClick={() => router.push("/recordings")}
+      />
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
