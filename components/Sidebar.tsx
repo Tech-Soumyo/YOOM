2400bf86bb5dcfe9e3021b0d7b7cbc6d44e1e46d
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-17 12:30:06 +0530

    load assets & sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      {[]}
+    </section>
+  );
+}
+
+export default Sidebar;
