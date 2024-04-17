23be7aee37426248d0ea63ded7ebf8bc68a9da5a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-17 20:38:06 +0530

    Clerk Auth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,24 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />;
-}
+const SigninPage = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center text-white bg">
+      <SignIn
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      />
+    </div>
+  );
+};
+export default SigninPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignUp />;
-}
+const SignUpPage = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <SignUp />
+    </div>
+  );
+};
+export default SignUpPage;
