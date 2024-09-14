32fa210bc4fe018410425e9b8dcbbe1298306ff1
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-15 00:49:41 +0530

    mainitaining the code

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,37 @@
-import { authMiddleware } from "@clerk/nextjs";
-// import {createRouteMatcher} from "@clerk/nextjs/"
+// import  authMiddleware  from "@clerk/nextjs";
+// // import {createRouteMatcher} from "@clerk/nextjs/"
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
+// // See https://clerk.com/docs/references/nextjs/auth-middleware
+// // for more information about configuring your Middleware
+// export default authMiddleware({
+//   // Allow signed out users to access the specified routes:
+//   // publicRoutes: ['/anyone-can-visit-this-route'],
+// });
+
+// export const config = {
+//   matcher: [
+//     // Exclude files with a "." followed by an extension, which are typically static files.
+//     // Exclude files in the _next directory, which are Next.js internals.
+//     "/((?!.+\\.[\\w]+$|_next).*)",
+//     // Re-include any files in the api or trpc folders that might have an extension
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
